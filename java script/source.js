3b3bd330dd2d716f40a420cb6d68550a16923c57
siyarampatel <siyarampateljbp@gmail.com>
2026-01-23 16:00:22 +0530

    port folio website

--- java script/source.js
+++ java script/source.js
@@ -1,8 +1,18 @@
 // js needs an compiler but we also have an inbuilt compiler in our browser
-console.log("hello");
 let btn = document.querySelector('button');
 let body = document.querySelector('body');
+let isDark=true;
+
+// btn.addEventListener("click",function(){
+//     body.style.backgroundColor='blue'
+// })
 
 btn.addEventListener("click",function(){
-    body.style.backgroundColor='blue'
+    if(isDark){
+        body.style.backgroundColor='black'
+    }
+    else{
+        body.style.backgroundColor='white'
+    }
+    isDark=!isDark
 })
