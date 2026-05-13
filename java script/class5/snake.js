ed9cd51d126ea47834f15f17a85adc50d61f528f
siyarampatel <siyarampateljbp@gmail.com>
2026-05-13 11:25:57 +0530

    snake

--- java script/class5/snake.js
+++ java script/class5/snake.js
@@ -3,6 +3,16 @@ let pen = canvas.getContext("2d");
 let snakeCell = [[0,0]]
 let cell = 50;
 let direction = "right";
+let gameOver = false;
+let score=0;
+
+let randomCell = generateR();
+let id = setInterval(()=>{
+    update();
+    draw();
+    
+},200)
+
 document.addEventListener("keydown",(e)=>{
     console.log(e);
     if(e.key=="ArrowDown"){
@@ -20,13 +30,29 @@ document.addEventListener("keydown",(e)=>{
 })
 
 function draw(){
+    if(gameOver==true){
+        clearInterval(id);
+        pen.fillStyle="snow";
+        pen.font="40px sans-serif";
+        pen.fillText("GAME OVER",190,230)
+        return;
+    }
+
     pen.fillStyle="red";
     pen.clearRect(0,0,600,500)
     for(let a of snakeCell){
         pen.fillRect(a[0],a[1],cell,cell);
     }
+
+    pen.fillStyle="pink";
+    pen.font="40px sans-serif";
+    pen.fillText(`${score}`,280,40);
+
+    pen.fillStyle="yellow";
+    pen.fillRect(randomCell[0],randomCell[1],cell,cell);
+
 }
-draw();
+
 function update(){
     let headX = snakeCell[snakeCell.length-1][0];
     let headY = snakeCell[snakeCell.length-1][1];
@@ -36,26 +62,45 @@ function update(){
     if(direction=="right"){
         newX=headX+cell;
         newY=headY;
+        if(newX >= 600){
+            gameOver=true;
+        }
     }
     else if(direction=="left"){
         newX=headX-cell;
         newY=headY;
+        if(newX < 0){
+            gameOver=true;
+        }
     }
     else if(direction=="down"){
         newX=headX;
         newY=headY+cell;
+        if(newY >= 500){
+            gameOver=true;
+        }
     }
     else{
         newX=headX;
         newY=headY-cell;
+        if(newY < 0){
+            gameOver=true;
+        }
     }
 
     snakeCell.push([newX,newY]);
-    snakeCell.shift();
+    if(newX==randomCell[0] && newY==randomCell[1]){
+        score++;
+        randomCell=generateR();
+    }else{
+        snakeCell.shift();
+    }
 }
 
-setInterval(()=>{
-    update();
-    draw();
-    
-},200)
+
+function generateR(){
+    return[
+        Math.floor(Math.random()*550/cell)*cell,
+        Math.floor(Math.random()*450/cell)*cell,
+    ]
+}
