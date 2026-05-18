fcafb3ee61de4f7670312e7b3c3a6524477f803b
siyarampatel <siyarampateljbp@gmail.com>
2026-05-18 13:32:21 +0530

    local storage

---
+++ java script/class9/1.js
@@ -0,0 +1,8 @@
+//feature for storing limited amount of data on browser 
+
+localStorage.setItem("user","rahul")//(key,data)
+// localStorage.removeItem("user");
+let data = localStor
+age.getItem("user")
+
+console.log(data)
