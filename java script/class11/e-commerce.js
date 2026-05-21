76844cea2d9d737bfa10d21e3d5db843d203f8bf
siyarampatel <siyarampateljbp@gmail.com>
2026-05-21 10:05:10 +0530

    file sharing

--- java script/class11/e-commerce.js
+++ java script/class11/e-commerce.js
@@ -2,7 +2,6 @@ fetch("https://dummyjson.com/products").then((res)=>{
     return res.json()
 
 }).then((data)=>{
-    console.log(data.products);
    let body= document.querySelector("body")
     data.products.map((a)=>{
         body.innerHTML+=`<div style="border:1px solid black;
---
+++ java script/class12/1.js
@@ -0,0 +1,8 @@
+// learning to share data b/w diffrent js files and only single js file will 
+// be directly connected to html file
+ 
+//the file which is directly connected to html will be used
+//for import and otheer files will be used for export
+
+import data from './1.1.js'
+console.log(data)
